cce8b5ebcbc199b4bbd402020974d2ead6621ba4
john longanecker <johnlonganecker@gmail.com>
2018-04-26 16:50:54 -0700

    moved functionality into one vue object

--- scripts/main.js
+++ scripts/main.js
@@ -1,87 +1,48 @@
-var isRunning = false,
-  time = 0;
-
-function start() {
-  isRunning = start;
-  timer();
-}
-
-function stop() {
-  isRunning = false;
-}
-
-function reset() {
-  isRunning = false;
-  time = 0;
-  document.getElementById('timerLabel').innerHTML = '00:00:00';
-}
-
-function timer() {
-  if(isRunning) {
-    setTimeout(function() {
-      time++;
-      var min = Math.floor(time/100/60),
-        sec = Math.floor(time/100),
-        milliSec = time % 100;
-
-      if(min < 10) {
-        min = "0" + min;
-      }
-      if(sec >= 60) {
-        sec = sec % 60;
-      }
-      if(sec < 10) {
-        sec = "0" + sec;
-      }
-      timer();
-      document.getElementById('timerLabel').innerHTML = min + ":" + sec + ":" + milliSec;
-    }, 10);
-  }
-}
-
-Vue.component('start-button', {
-  props: ['name'],
-  methods: {
-    start: function() {
-      start();
-    },
-  },
-  template: `<button v-on:click="start">{{ name }}</button>`
-});
-
-Vue.component('stop-button', {
-  props: ['name'],
-  methods: {
-    stop: function() {
-      stop();
-    },
-  },
-  template: `<button v-on:click="stop">{{ name }}</button>`
-});
-
-Vue.component('reset-button', {
-  props: ['name'],
-  methods: {
-    reset: function() {
-      reset();
-    },
-  },
-  template: `<button v-on:click="reset">{{ name }}</button>`
-});
-
-Vue.component('lap-button', {
-  props: ['name'],
-  methods: {
-    lap: function() {
-      console.log('lap');
-    },
-  },
-  template: `<button v-on:click="lap">{{ name }}</button>`
-});
-
 let app = new Vue({
   el: '#app',
   data: {
-    message: 'Stop Watch'
+    message: 'Stop Watch',
+    isRunning: false,
+    time: 0,
+    timeLabel: "00:00:00",
+    intervalId: null
+  },
+  methods: {
+    start: function() {
+      this.timer();
+      this.isRunning = true;
+    },
+    stop: function() {
+      this.isRunning = false;
+      clearInterval(this.intervalId);
+    },
+    reset: function() {
+      this.isRunning = false;
+      clearInterval(this.intervalId);
+      this.time = 0;
+      this.timeLabel = "00:00:00";
+    },
+    timer: function() {
+      var that = this;
+      if(!this.isRunning) {
+        that.intervalId = setInterval(function() {
+          that.time++;
+          var min = Math.floor(that.time/100/60),
+            sec = Math.floor(that.time/100),
+            milliSec = that.time % 100;
+
+          if(min < 10) {
+            min = "0" + min;
+          }
+          if(sec >= 60) {
+            sec = sec % 60;
+          }
+          if(sec < 10) {
+            sec = "0" + sec;
+          }
+          that.timeLabel = min + ":" + sec + ":" + milliSec;
+        }, 10);
+      }
+    }
   }
 });
